36e377260d80aa6575bd764535173d17eae8c53a
Squirrellzy <78796513+Squirrellzy@users.noreply.github.com>
2025-05-28 14:17:51 -0400

    Update cc_logger_app.py

--- cc_logger_app.py
+++ cc_logger_app.py
@@ -38,7 +38,7 @@ st.markdown("<h2 class='title-wrapper'>Collection Conveyor Tracker – Indy</h2>
 # Add labels for dropdown columns
 st.markdown("**(A)-1**   **2**   **3**   **4-(B)**")
 
-# Display editable table
+# Display editable table with dropdowns and comment field only
 edited_df = st.data_editor(
     st.session_state.df,
     column_config={
@@ -51,6 +51,7 @@ edited_df = st.data_editor(
     use_container_width=True,
     num_rows="fixed",
     hide_index=True,
+    disabled=["CC#"]
 )
 
 # GitHub secrets
